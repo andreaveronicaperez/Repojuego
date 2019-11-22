fc8ef39aacd32da2fdaeb8ba85d004c9b3f33935
AGUSTINAIA <escribilotodojunto@yahoo.com.ar>
2019-11-22 00:00:12 -0300

    se contnuo validando el formulario de registro y retoques de formato

--- javascript/registro.js
+++ javascript/registro.js
@@ -4,10 +4,22 @@ window.addEventListener('load',function(){
     var errores= false;
     var email= document.querySelector("input.email");
     var nombre= document.querySelector("input.valida-nombre");
+    var apellido= document.querySelector("input.valida-apellido");
+    var apodo= document.querySelector("input.valida-apodo");
+    var contraseña= document.querySelector("input.valida-contraseña");
+    var recontraseña= document.querySelector("input.valida-recontraseña");
+    var provincia= document.querySelector("input.valida-provincia");
 
     var errorEmail= document.querySelector("span.errorEmail");
     var errorNombre= document.querySelector("span.errorNombre");
+    var errorApellido= document.querySelector("span.errorApellido");
+    var errorApodo= document.querySelector("span.errorApodo");
+    var errorContraseña= document.querySelector("span.errorContraseña");
+    var errorReContraseña= document.querySelector("span.errorReContraseña");
+    var errorProvincia= document.querySelector("span.errorProvincia");
+
     var regexMail= /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    var regexContraseña = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
 
   //  email.onblur=function(){
       if(email.value == ''){
@@ -36,6 +48,72 @@ window.addEventListener('load',function(){
     }else {
       errorNombre.innerHTML= "";
       nombre.style.border= "1px solid green";
+    }
+
+
+    if(apellido.value == ""){
+      errorApellido.innerHTML= "Este campo es obligatorio";
+      apellido.style.border= "1px solid red";
+      errores=true;
+    }else if( apellido.value.length <2){
+      errorApellido.innerHTML= "Debe contener al menos 2 caracteres"
+      apellido.style.border= "1px solid red";
+      errores= true;
+    }else {
+      errorApellido.innerHTML= "";
+      apellido.style.border= "1px solid green";
+    }
+
+
+    if(apodo.value == ""){
+      errorApodo.innerHTML= "Este campo es obligatorio";
+      apodo.style.border= "1px solid red";
+      errores=true;
+    }else if( apodo.value.length <2){
+      errorApodo.innerHTML= "Debe contener al menos 2 caracteres"
+      apodo.style.border= "1px solid red";
+      errores= true;
+    }else {
+      errorApodo.innerHTML= "";
+      apodo.style.border= "1px solid green";
+    }
+
+
+    if(contraseña.value == ""){
+      errorContraseña.innerHTML= "Este campo es obligatorio";
+      contraseña.style.border= "1px solid red";
+      errores=true;
+    }else if( contraseña.value.length <5){
+      errorContraseña.innerHTML= "Debe contener al menos 5 caracteres"
+      contraseña.style.border= "1px solid red";
+      errores= true;
+    }else {
+      errorContraseña.innerHTML= "";
+      contraseña.style.border= "1px solid green";
+    }
+
+
+    if(recontraseña.value == ""){
+      errorReContraseña.innerHTML= "Este campo es obligatorio";
+      recontraseña.style.border= "1px solid red";
+      errores=true;
+    }else if( recontraseña.value != contraseña.value){
+      errorReContraseña.innerHTML= "Debe ser Igual a la contraseña ingresada"
+      recontraseña.style.border= "1px solid red";
+      errores= true;
+    }else {
+      errorReContraseña.innerHTML= "";
+      recontraseña.style.border= "1px solid green";
+    }
+
+
+    if(provincia.value != "Selecciona una provincia"){
+      errorProvincia.innerHTML= "Debe seleccionar una provincia";
+      provincia.style.border= "1px solid red";
+      errores=true;
+    }else {
+      errorReContraseña.innerHTML= "";
+      recontraseña.style.border= "1px solid green";
     }
 
 
